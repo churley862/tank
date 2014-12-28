19ecbf00cc27c88c4b88680569262d4ceef03961
Patrick Hurley <phurley@gmail.com>
2014-12-27 20:29:14 -0500

    Initial Commit

---
+++ tank/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  tank
+//
+//  Created by Kitchen on 12/27/14.
+//  Copyright (c) 2014 Collin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
